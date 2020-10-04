923b039c593927ff5f33eeef9313f10e2e18ea46
Florian Bruggisser <florian@bildspur.ch>
2020-10-04 18:34:27 +0200

    added support for different extraction method

--- sharp-frame-extractor.py
+++ sharp-frame-extractor.py
@@ -8,7 +8,7 @@ import numpy as np
 debug = False
 
 
-def extractImages(video_file, output_path, window_size_ms, min_sharpness, output_format, crop_factor):
+def extractImages(video_file, output_path, window_size_ms, min_sharpness, output_format, crop_factor, extraction_method):
     count = 0
     vidcap = cv2.VideoCapture(video_file)
     vidcap.read()
@@ -33,7 +33,7 @@ def extractImages(video_file, output_path, window_size_ms, min_sharpness, output
             "analyzing batch %d/%d (%.2fs to %.2fs)..." % (i, step_count, window_start_ms / 1000, window_end_ms / 1000))
 
         # extracting frames and getting the one with the best metric
-        frames = extract_frame_batch(vidcap, window_start_ms, window_size_ms, crop_factor)
+        frames = extract_frame_batch(vidcap, window_start_ms, window_size_ms, crop_factor, extraction_method)
 
         if len(frames) == 0:
             print("ERROR: No frames extracted (maybe a video error!)")
@@ -57,51 +57,7 @@ def extractImages(video_file, output_path, window_size_ms, min_sharpness, output
         count += 1
 
 
-def extract_sharpness_enhanced(frame_index, frame):
-    Gx = cv2.Sobel(frame, cv2.CV_32F, 1, 0)
-    Gy = cv2.Sobel(frame, cv2.CV_32F, 0, 1)
-
-    normGx = cv2.norm(Gx)
-    normGy = cv2.norm(Gy)
-
-    height, width, channels = frame.shape
-
-    sumSq = normGx * normGx + normGy * normGy
-    sharpness = 1. / (sumSq / (height * width) + 1e-6)
-    return (1.0 - sharpness) * 100, 0, 0
-
-
-def extract_sharpness(frame_index, frame):
-    # extract best parameters for canny
-    v = np.median(frame)
-
-    sigma = 0.33
-    lower = int(max(0, (1.0 - sigma) * v))
-    upper = int(min(255, (1.0 + sigma) * v))
-
-    # detect mean and standard deviation
-    edges = cv2.Canny(frame, lower, upper)
-    mean, std = cv2.meanStdDev(edges)
-
-    # unpack values
-    mean = mean[0][0]
-    std = std[0][0]
-
-    sharpness = mean * std
-
-    if debug:
-        text = "Frame #%d Sharpness=%.2fs Mean=%.2fs StdDev=%.2fs" % (frame_index, sharpness, mean, std)
-        print("Canny: L=%d H=%d" % (lower, upper))
-        print(text)
-        colored_edges = cv2.merge((edges, edges, edges))
-        cv2.putText(colored_edges, text, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-        cv2.imshow("Preview", colored_edges)
-        cv2.waitKey(0)
-
-    return sharpness, mean, std
-
-
-def extract_frame_batch(vidcap, start_ms, window_ms, crop_factor):
+def extract_frame_batch(vidcap, start_ms, window_ms, crop_factor, extraction_method):
     results = []
 
     # jump to video start
@@ -140,12 +96,55 @@ def extract_frame_batch(vidcap, start_ms, window_ms, crop_factor):
             image = image[y:y + ch, x:x + cw]
 
         # extract metrics
-        sharpness, mean, std = extract_sharpness_enhanced(frame_index, image)
+        sharpness, mean, std = extraction_method(frame_index, image)
         results.append((frame_index, sharpness, mean, std))
 
         frame_available = True
 
     return results
+
+def extract_sharpness_sobel(frame_index, frame):
+    Gx = cv2.Sobel(frame, cv2.CV_32F, 1, 0)
+    Gy = cv2.Sobel(frame, cv2.CV_32F, 0, 1)
+
+    normGx = cv2.norm(Gx)
+    normGy = cv2.norm(Gy)
+
+    height, width, channels = frame.shape
+
+    sumSq = normGx * normGx + normGy * normGy
+    sharpness = 1. / (sumSq / (height * width) + 1e-6)
+    return (1.0 - sharpness) * 100, 0, 0
+
+
+def extract_sharpness_canny(frame_index, frame):
+    # extract best parameters for canny
+    v = np.median(frame)
+
+    sigma = 0.33
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+
+    # detect mean and standard deviation
+    edges = cv2.Canny(frame, lower, upper)
+    mean, std = cv2.meanStdDev(edges)
+
+    # unpack values
+    mean = mean[0][0]
+    std = std[0][0]
+
+    sharpness = mean * std
+
+    if debug:
+        text = "Frame #%d Sharpness=%.2fs Mean=%.2fs StdDev=%.2fs" % (frame_index, sharpness, mean, std)
+        print("Canny: L=%d H=%d" % (lower, upper))
+        print(text)
+        colored_edges = cv2.merge((edges, edges, edges))
+        cv2.putText(colored_edges, text, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow("Preview", colored_edges)
+        cv2.waitKey(0)
+
+    return sharpness, mean, std
 
 
 if __name__ == "__main__":
@@ -156,8 +155,13 @@ if __name__ == "__main__":
     a.add_argument("--min", default=60, help="Minimum sharpness level")
     a.add_argument("--format", default=".jpg", help="Frame output format")
     a.add_argument("--crop", default=0.25, help="Crop to center ROI for sharpness detection")
+    a.add_argument("--method", default="canny", help="Extraction algorithm (canny / sobel")
     a.add_argument("--debug", default=False, help="Shows debug frames and information")
     args = a.parse_args()
 
+    extraction_method = extract_sharpness_canny
+    if args.method == "sobel":
+    	extraction_method = extract_sharpness_sobel
+
     debug = bool(args.debug)
-    extractImages(args.video, args.output, int(args.window), int(args.min), args.format, float(args.crop))
+    extractImages(args.video, args.output, int(args.window), int(args.min), args.format, float(args.crop), extraction_method)
