3f5abec13c0ded7429966221d08bc389fbc849cb
Florian Bruggisser <florian@bildspur.ch>
2021-04-24 18:04:12 +0200

    fixed required packages

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ NAME = 'sharp_frame_extractor'
 
 required_packages = find_packages()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name=NAME,
-    version='1.6.3',
+    version='1.6.4',
     packages=required_packages,
     url='https://github.com/cansik/sharp-frame-extractor',
     license='MIT License',
     author='Florian Bruggisser',
     author_email='github@broox.ch',
     description='Extracts sharp frames from a video by using a time window to detect the sharpest frame.',
-    install_requires=['wheel', 'opencv-python', 'numpy', 'psutil'],
+    install_requires=required,
 )
