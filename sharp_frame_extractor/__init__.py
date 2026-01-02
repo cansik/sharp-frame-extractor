7ffd936beb84df34536bfd228a1c5de8bfb6a665
Florian Bruggisser <github@broox.ch>
2026-01-02 14:09:06 +0100

    cleaned project

--- sharp_frame_extractor/__init__.py
+++
@@ -1,7 +0,0 @@
-from sharp_frame_extractor.estimator.CannyEstimator import CannyEstimator
-from sharp_frame_extractor.estimator.SobelEstimator import SobelEstimator
-
-DefaultEstimators = {
-    "canny": CannyEstimator(),
-    "sobel": SobelEstimator()
-}
